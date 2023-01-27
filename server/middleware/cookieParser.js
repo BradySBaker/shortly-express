f24f6a1c5536b1ec574e92349124878e34e946f3
kyle <kyle.dick0811@gmail.com>
2023-01-26 20:41:00 -0700

    Complete Adding Routing Callbacks

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,24 @@
 const parseCookies = (req, res, next) => {
+  //console.log('HEADERS! ', req.headers);
+  if (req.headers.cookie) {
+    var cookies = req.headers.cookie.split(' ');
+    console.log('ARRAY', cookies);
+    cookies.forEach((cookie) => {
+      //Split var curCookie = [shortlyid, cookieDetails]
+      var cookieDetails = cookie.split('=');
+      //Check cookieDetails for semiColon
+      var key = cookieDetails[0];
+      var value = cookieDetails[1];
+      //If semiColon splice to end - 1
+      if (value[value.length - 1] === ';') {
+        value = value.slice(0, value.length - 1);
+      }
+      req.cookies[key] = value;
+    });
+  }
+  console.log(req.cookies);
+  //console.log('REQUEST', req);
+  next();
 };
 
 module.exports = parseCookies;
